7b6abcd22e6b74298ab5344e0ee59df07a22449b
Patrick Truong <pattruong@gmail.com>
2018-02-20 08:49:24 -0800

    Added Template for Chris + Amber

---
+++ fallingObjects.js
@@ -0,0 +1,17 @@
+// *********************************************************
+// *********************************************************
+// *********************************************************
+// ****************** Chris' Section ***********************
+// *********************************************************
+// ******************* Falling Objects Class ***************
+// *********************************************************
+
+
+
+// 1. Build a FallingObject class with attributes: {type, damage, points, etc}
+
+
+
+// 2. Build functions that allow you to make a lot of FallingObjects easy
+
+// ex. function makeFallingObjects(number) --> returns array of n-amount of FallingObjects
