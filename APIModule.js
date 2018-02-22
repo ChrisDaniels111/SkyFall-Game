ef928033d5d90f6287ba959d4a7e060a8b697846
pattruong <pattruong@gmail.com>
2018-02-21 21:58:52 -0800

    added pokemon api for selecting

--- APIModule.js
+++ APIModule.js
@@ -1,28 +1,90 @@
 // starter pokemons [bulbasaur, charmander, squirtle]
 // starter pokemon images
 
-var individualPokeURL = "https://pokeapi.co/api/v2/pokemon/";
-var imagePokeURL = "https://pokeapi.co/api/v2/pokemon-form/";       //response.sprites.front_default
+
+var individualPokeURL = "https://pokeapi.co/api/v2/pokemon/";       //response.sprites.front_default
+var imagePokeURL = "https://pokeapi.co/media/sprites/pokemon/.png"
+
 
 var pokemonNames = ["bulbasaur", "charmander", "squirtle"];
 
-var pokemonsIMG = {}
+var pokemonFront;
+var pokemonType;
 
-
-function getPokemonIMG(name) {
+function getPokemonIMG() {
     $.ajax({
-        url: imagePokeURL + name,
-        method: "GET"
+        url: individualPokeURL + $("input").val().toLowerCase(),
+        method: "GET",
+        statusCode: {
+            404: function() {
+                alert('invalid pokemon');
+            }
+        }
     })
     .done(function(response) {
-        console.log(response);
+        var pokemonData = response;
+
+        pokemonFront = `<img src="https://pokeapi.co/media/sprites/pokemon/${pokemonData.id}.png" id="pokemonFrontPage">`;
+
+
+        $("#pokemon").empty(); // clear div
+        $("#pokemon").append(pokemonFront);
+        $("#pokemon").append(listPokemonType(pokemonData));
+        
     });
 }
 
-console.log(individualPokeURL + pokemonNames[1]);
-getPokemon(pokemonNames[1]);
+
+
+// console.log(individualPokeURL + pokemonNames[1]);
+
+
+function listPokemonType(pokemonData) {
+    var message = 'Type: ';
+    var types = pokemonData.types;
+    for (var i = 0; i < types.length; i++) {
+        var typeName = types[i]['type']['name'];
+        message += capitalizeFirstLetter(typeName) + ' ';
+    }
+    return `<p>${message}</p>`;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+// append an to page for pokemon
+// load screen
+// show selected pokemon
 
 
 
+// build function to append a box to div with input
 
-// need an append items to page button 
+function createPokemonInputBox() {
+    var $label = $("<label>", {"for": "pokemon-input"});
+    var $div = $("<div>", {"class": "input-group"});
+    var $input = $("<input>", {"type": "text", "class": "form-control",
+                   "placeholder": "Search for a pokemon..."});
+    var $span = $("<span>", {"class": "input-group-btn"});
+    var $button = $("<button>", {"class": "btn btn-primary", "type": "button", "text": "Go!"});
+
+    $button.click(getPokemonIMG)
+
+    $span.append($button);
+    $div.append($input);
+    $div.append($span);
+
+    // <div class="input-group">
+    //   <input type="text" class="form-control" placeholder="Search for...">
+    //   <span class="input-group-btn">
+    //     <button class="btn btn-secondary" type="button">Go!</button>
+    //   </span>
+    // </div>
+
+    $("#pokemon").append($label);
+    $("#pokemon").append($div);
+}
+
+createPokemonInputBox();
