f220baf81ed8d7d1f1750e5f69d8b4f0913559a8
Patrick Truong <pattruong@gmail.com>
2018-02-21 17:52:20 -0800

    Added Pokemon Testing

--- APIModule.js
+++ APIModule.js
@@ -1,2 +1,27 @@
 // starter pokemons [bulbasaur, charmander, squirtle]
 // starter pokemon images
+
+var individualPokeURL = "https://pokeapi.co/api/v2/pokemon/";
+var imagePokeURL = "https://pokeapi.co/api/v2/pokemon-form/";       //response.sprites.front_default
+
+var pokemonNames = ["bulbasaur", "charmander", "squirtle"];
+
+var pokemons = {
+    // bulbasaur: ,
+    // charmander: ,
+    // squirtle: 
+}
+
+
+function getPokemon(name) {
+    $.ajax({
+        url: individualPokeURL + name,
+        method: 'GET'
+    })
+    .done(function(response) {
+        console.log(response.name);
+    });
+}
+
+console.log(individualPokeURL + pokemonNames[1]);
+getPokemon(pokemonNames[1]);
