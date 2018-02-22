78e3da97c6fda2634753fd5040b2682d7a5896bc
Patrick Truong <pattruong@gmail.com>
2018-02-21 18:01:02 -0800

    minor change Pokemon

--- APIModule.js
+++ APIModule.js
@@ -6,22 +6,23 @@ var imagePokeURL = "https://pokeapi.co/api/v2/pokemon-form/";       //response.s
 
 var pokemonNames = ["bulbasaur", "charmander", "squirtle"];
 
-var pokemons = {
-    // bulbasaur: ,
-    // charmander: ,
-    // squirtle: 
-}
+var pokemonsIMG = {}
 
 
-function getPokemon(name) {
+function getPokemonIMG(name) {
     $.ajax({
-        url: individualPokeURL + name,
-        method: 'GET'
+        url: imagePokeURL + name,
+        method: "GET"
     })
     .done(function(response) {
-        console.log(response.name);
+        console.log(response);
     });
 }
 
 console.log(individualPokeURL + pokemonNames[1]);
 getPokemon(pokemonNames[1]);
+
+
+
+
+// need an append items to page button 
