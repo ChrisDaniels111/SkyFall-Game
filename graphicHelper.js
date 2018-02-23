25dc55bc1f1940037d4c5726f1cdef4c606c0ea4
Patrick Truong <pattruong@gmail.com>
2018-02-23 09:56:05 -0800

    documented and organized graphicHelper.js

--- graphicHelper.js
+++ graphicHelper.js
@@ -1,37 +1,36 @@
-
-
-// create individual div for each user name
-
-function addPlayerGameData() {
-    var playerData = {};
-    playerData.username = username;
-    playerData.score = player0.points;
-    arrayOfPlayers.push(playerData);
-    arrayOfPlayers.sort((a, b) => a.score < b.score);
-}
-
-function appendEndScoreToPage() {
-    $("#score-box").empty();            // clear the current data
-    for (var i = 0; i < arrayOfPlayers.length; i++) {
-        var playerData = arrayOfPlayers[i];
-        var text = `${playerData.username} ${playerData.score}`
-        var $div = $("<div>", {"id": `score${i}`});
-        var $h3 = $("<h3>", {"text": text});
-        $div.append($h3);
-        $("#score-box").append($div);
-    }
-}
-
-// create 20 sprites that shoot randomly from bottom of page to top; deletes any sprites it contacts; some add points
+var buttonExist;
+var specialMoveImg;
 
 var specialMoveArray = [];
-var buttonExist;
+
 var specialMoveDict = {
     grass: './images/grass2.png',
     fire: './images/fire-special2.png',
-    default: './images/water2.png'
+    water: './images/water2.png',
+    ghost: './images/boo.png',
+    default: './images/fire.png'
 }
-var specialMoveImg;
+
+
+
+///////////////////////////////////////////////////////////
+/////////////// CALL THIS IN START BUTTON /////////////////
+///////////////////////////////////////////////////////////
+
+function createSpecialMoveButton() {
+    var $button = $("<button>", {"class": "btn btn-primary", "id": "special", "type": "button", "text": "Special Ready"});
+    $button.click(makeSpecialSprites);
+    $('.game').append($button);
+    buttonExist = true;
+}
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+
+// ********************************************************
+// ****************** Special Sprite **********************
+// ********************************************************
 
 function makeSpecialSprites() {
     // make 25 sprites
@@ -39,7 +38,6 @@ function makeSpecialSprites() {
     for (var i = 0; i < 25; i++) {
         var specialSprite = new Component(20, 20, specialMoveImg, 185, 300, 'image');
         specialMoveArray.push(specialSprite);
-        console.log("HERE:", specialMoveArray);
     }
     buttonExist = false;
     $("#special").remove();
@@ -59,6 +57,13 @@ function updateSpecialMove() {
     specialMoveArray = specialMoveArray.filter(sprite => sprite.y >= 0);    // remove sprites out of box
 }
 
+
+// ********************************************************
+// ****************** Special Sprite **********************
+// ********************************************************
+// ****************** Helper Functions ********************
+// ********************************************************
+
 function typeImageSelector() {
     var found = false;
     var getType = pokemonType.toLowerCase().split(' ');
@@ -70,29 +75,15 @@ function typeImageSelector() {
         if (key in specialMoveDict && !found) {
             specialMoveImg = specialMoveDict[key];
             found = true;
-        } else if (!(key in specialMoveDict) && found) {
-            continue;
         } else if (key in specialMoveDict && found) {
             specialMoveImg = specialMoveDict[key];
+        } else if (!(key in specialMoveDict) && found) {
+            continue;
         } else {
             specialMoveImg = specialMoveDict['default'];
         }
     }
 }
-
-///////////////////////////////////////////////////////////
-/////////////// CALL THIS IN START BUTTON /////////////////
-///////////////////////////////////////////////////////////
-
-function createSpecialMoveButton() {
-    var $button = $("<button>", {"class": "btn btn-primary", "id": "special", "type": "button", "text": "Special Ready"});
-    $button.click(makeSpecialSprites);
-    $('.game').append($button);
-    buttonExist = true;
-}
-
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
 
 function specialSpeedX() {
     var num = Math.random() * 20; // this will get a number between 1 and 99;
@@ -102,4 +93,29 @@ function specialSpeedX() {
 
 function specialSpeedY() {
     return -(Math.random() * 4);
+}
+
+
+//////////////////////////////////////////////////////////
+//////////// Appends Username to Scoreboard //////////////
+//////////////////////////////////////////////////////////
+
+function addPlayerGameData() {
+    var playerData = {};
+    playerData.username = username;
+    playerData.score = player0.points;
+    arrayOfPlayers.push(playerData);
+    arrayOfPlayers.sort((a, b) => a.score < b.score);
+}
+
+function appendEndScoreToPage() {
+    $("#score-box").empty();            // clear the current data
+    for (var i = 0; i < arrayOfPlayers.length; i++) {
+        var playerData = arrayOfPlayers[i];
+        var text = `${playerData.username} ${playerData.score}`
+        var $div = $("<div>", {"id": `score${i}`});
+        var $h3 = $("<h3>", {"text": text});
+        $div.append($h3);
+        $("#score-box").append($div);
+    }
 }
